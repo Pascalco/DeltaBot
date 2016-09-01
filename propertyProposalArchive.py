c7fab8ab174ad5deccfd965bcb26f5f6783e2c4c
Pascalco <pascal.leimer@bluewin.ch>
2016-09-01 10:22:20 +0200

    handle proposal pages with multiple proposals

--- propertyProposalArchive.py
+++ propertyProposalArchive.py
@@ -13,17 +13,17 @@ site = pywikibot.Site('wikidata','wikidata')
 #remove proposals from category pages
 def removeProposals(proposals):
     categories = {}
-    for proposal in proposals:       
+    for proposal in proposals:
         if proposal['category'] not in categories:
             page = pywikibot.Page(site,'Wikidata:Property_proposal/'+proposal['category'])
             te = re.sub(r'(?<!_)_(?!_)',' ',page.get())
-            categories[proposal['category']] = {'text': te, 'count': 0}         
+            categories[proposal['category']] = {'text': te, 'count': 0}
         categories[proposal['category']]['text'] = re.sub(r'{{Wikidata:Property proposal/'+proposal['name']+'}}\n?','',categories[proposal['category']]['text'])
         categories[proposal['category']]['count'] += 1
     for category in categories:
         page = pywikibot.Page(site,'Wikidata:Property_proposal/'+category)
         comment = 'archiving '+str(categories[category]['count'])+' proposals' if categories[category]['count'] != 1 else 'archiving 1 proposal'
-        page.put(categories[category]['text'],comment=comment,minorEdit=False)  
+        page.put(categories[category]['text'],comment=comment,minorEdit=False)
 
 #load new achive subpage
 def loadNewArchivePage(archive):
@@ -45,7 +45,7 @@ def loadNewArchivePage(archive):
         page.put(newarchive,comment='new archive',minorEdit=False)
     return page.get().replace('_',' ')
 
-#add proposals to archive    
+#add proposals to archive
 def updateArchive(proposals):
     proposals.sort(key=lambda x:x['closedate'])
     archives = {}
@@ -67,7 +67,13 @@ def updateArchive(proposals):
     for archive in archives:
         page = pywikibot.Page(site,'Wikidata:Property_proposal/Archive/'+archive)
         comment = 'archiving '+str(archives[archive]['count'])+' proposals' if archives[archive]['count'] != 1 else 'archiving 1 proposal'
-        page.put(archives[archive]['text'],comment=comment,minorEdit=False)    
+        page.put(archives[archive]['text'],comment=comment,minorEdit=False)
+
+def allClosed(stati):
+    for status in stati:
+        if not status.isdigit() and status.lower() != 'not done' and status.lower() != 'withdrawn':
+            return False
+    return True
 
 def main():
     toArchive = []
@@ -84,27 +90,26 @@ def main():
             else:
                 newname = proposal
             pptext = re.sub(r'(<!([^>]+)>)|\n','',page2.get())
-            if pptext.count('{{Property proposal') > 1: #ToDo: handle pages with multiple proposal
+            stati = re.findall('\|\s*status\s*=\s*([^\|\}]+)',pptext)
+            stati = map(unicode.strip, stati)
+            if not allClosed(stati):
                 continue
-            res = re.search('\|\s*status\s*=\s*([^\|\}]+)',pptext)
-            if res:
-                status = res.group(1).strip()
-                if status.isdigit() or status.lower() == 'not done' or status.lower() == 'withdrawn':
-                    status = status if status != 'not done' else ''
-                    history = page2.getVersionHistory()
-                    if (today - history[0].timestamp).days >= 3:
-                        month = str(history[0].timestamp.month) if history[0].timestamp.month > 9 else '0'+str(history[0].timestamp.month)
-                        data = {
-                            'name': proposal.replace('_',' '),
-                            'newname': newname,
-                            'category': category,
-                            'proposer': history[-1].user,
-                            'startdate': history[-1].timestamp.date().isoformat(),
-                            'closedate': history[0].timestamp.date().isoformat(),
-                            'note': status,
-                            'archive': str(history[0].timestamp.year)+'/'+month
-                        }
-                        toArchive.append(data)
+            for status in stati:
+                status = status if status != 'not done' else ''
+                history = page2.getVersionHistory()
+                if (today - history[0].timestamp).days >= 3:
+                    month = str(history[0].timestamp.month) if history[0].timestamp.month > 9 else '0'+str(history[0].timestamp.month)
+                    data = {
+                        'name': proposal.replace('_',' '),
+                        'newname': newname,
+                        'category': category,
+                        'proposer': history[-1].user,
+                        'startdate': history[-1].timestamp.date().isoformat(),
+                        'closedate': history[0].timestamp.date().isoformat(),
+                        'note': status,
+                        'archive': str(history[0].timestamp.year)+'/'+month
+                    }
+                    toArchive.append(data)
     if len(toArchive) > 0:
         updateArchive(toArchive)
         removeProposals(toArchive)
