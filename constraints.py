fc5bfe1dc67ffcb02a94f2df2d81c2df6932bf23
Pascalco <pascal.leimer@bluewin.ch>
2018-03-20 00:42:49 +0100

    read constraints from statements

--- constraints.py
+++ constraints.py
@@ -13,14 +13,14 @@ import requests
 import json
 import re
 import time
-import mwparserfromhell
 import pywikibot
 from pywikibot.pagegenerators import *
 
 site = pywikibot.Site('wikidata', 'wikidata')
 repo = site.data_repository()
 
-types = ['Constraint:Type', 'Constraint:Value type', 'Constraint:Symmatric', 'Constraint:Inverse', 'Constraint:Unique value', 'Constraint:Single value', 'Constraint:Multi value', 'Constraint:Item', 'Constraint:Target required claim', 'Constraint:Units', 'Constraint:Range', 'Constraint:Qualifiers', 'Constraint:Value only', 'Constraint:Qualifier', 'Constraint:Source only', 'Constraint:Format', 'Constraint:One of']
+types = ['Q21503250', 'Q21510865', 'Q21510862', 'Q21510855', 'Q21502410', 'Q19474404', 'Q21510857', 'Q21503247', 'Q21510864', 'Q21514353', 'Q21510860', 'Q21510851', 'Q21510856', 'Q21528958', 'Q21510863', 'Q21528959', 'Q21502404', 'Q21510859']
+#missing_types = ['Q21510852','Q21502838', 'Q21510854']
 header = '{{{{Constraint violations report|date={}|item count={}}}}}\n'
 
 
@@ -40,123 +40,130 @@ def requestquery(query, l=0):
     return r.json(), l
 
 
-def oneConstraint(p, datatype, template):
+def oneConstraint(p, datatype, constraint):
     # make list of exceptions
-    if template.has('exceptions'):
-        exceptions = template.get('exceptions').value.strip().replace('[[', '').replace(']]', '').split(',')
-        exceptions = map(unicode.strip, exceptions)
+    if 'P2303' in constraint.qualifiers:
+        exceptions = [val.getTarget().getID() for val in constraint.qualifiers['P2303']]
     else:
         exceptions = []
 
+    constrainttype = constraint.getTarget().getID()
+
     # write query
-    if template.name == 'Constraint:Type':
-        relation = 'P279' if template.get('relation').value == 'subclass' else 'P31'
-        list = template.get('class').value.split(',') if template.has('class') else template.get('classes').value.split(',')
-        list = map(unicode.strip, list)
+    if constrainttype == 'Q21503250': # Constraint:Type
+        relation = 'P279' if constraint.qualifiers['P2309'] == 'Q21514624' else 'P31' #ToDo: instance or subclass (Q30208840)
+        list = [val.getTarget().getID() for val in constraint.qualifiers['P2308']]
         classes = 'wd:' + ', wd:'.join(list)
         query = 'SELECT DISTINCT ?item WHERE{{ ?item wdt:{p} [] . MINUS {{ ?item wdt:{relation}/wdt:P279* ?class . FILTER(?class IN ({classes})) }} }} ORDER BY ?item'.format(p=p, relation=relation, classes=classes)
         title = '<span id="Type {}"></span>\n== "Type {{{{Q|{}}}}}" violations =='.format(', '.join(list), '}}, {{Q|'.join(list))
 
-    elif template.name == 'Constraint:Value type':
-        relation = 'P279' if template.get('relation').value == 'subclass' else 'P31'
-        list = template.get('class').value.split(',') if template.has('class') else template.get('classes').value.split(',')
-        list = map(unicode.strip, list)
+    elif constrainttype == 'Q21510865': # Constraint:Value type
+        relation = 'P279' if constraint.qualifiers['P2309'] == 'Q21514624' else 'P31' #ToDo: instance or subclass (Q30208840)
+        list = [val.getTarget().getID() for val in constraint.qualifiers['P2308']]
         classes = 'wd:' + ', wd:'.join(list)
         query = 'SELECT DISTINCT ?item WHERE{{ ?item wdt:{p} ?value . MINUS {{ ?value wdt:{relation}/wdt:P279* ?class . FILTER(?class IN ({classes})) }} }} ORDER BY ?item'.format(p=p, relation=relation, classes=classes)
         title = '<span id="Value type {}"></span>\n== "Value type {{{{Q|{}}}}}" violations =='.format(','.join(list), '}}, {{Q|'.join(list))
 
-    elif template.name == 'Constraint:Symmetric':
-        query = 'SELECT DISTINCT ?item { ?item wdt:{p} ?value . MINUS {{ ?value wdt:{p} ?item . }} }} ORDER BY ?item'.format(p=p)
+    elif constrainttype == 'Q21510862': # Constraint:Symmetric
+        query = 'SELECT DISTINCT ?item {{ ?item wdt:{p} ?value . MINUS {{ ?value wdt:{p} ?item . }} }} ORDER BY ?item'.format(p=p)
         title = '<span id="Symmetric"></span>\n== "Symmetric" violations =='
 
-    elif template.name == 'Constraint:Inverse':
-        property = template.get('property').value
+    elif constrainttype == 'Q21510855': # Constraint:Inverse
+        property = constraint.qualifiers['P2306'][0].getTarget().getID()
         query = 'SELECT DISTINCT ?item WHERE {{ ?inverse wdt:{p} ?item FILTER NOT EXISTS {{ ?item wdt:{property} ?inverse }} }} ORDER BY ?item'.format(p=p, property=property)
         title = '<span id="Inverse"></span>\n== "Inverse" violations =='
 
-    elif template.name == 'Constraint:Unique value':
+    elif constrainttype == 'Q21502410': # Constraint:Unique value/Distinct value
         query = 'SELECT DISTINCT ?item (COUNT(?item1) AS ?count)(GROUP_CONCAT(?item1;separator=",") AS ?items) WHERE {{ ?item1 wdt:{p} ?item }} GROUP BY ?item HAVING(?count > 1) ORDER BY ?item1'.format(p=p)
         title = '<span id="Unique value"></span>\n== "Unique value" violations =='
         variables = ['items']
 
-    elif template.name == 'Constraint:Single value':
+    elif constrainttype == 'Q19474404': # Constraint:Single value
         query = 'SELECT DISTINCT ?item (COUNT(?value) AS ?count)(GROUP_CONCAT(?value;separator=", ") AS ?values) WHERE {{ ?item wdt:{p} ?value }} GROUP BY ?item HAVING(?count > 1) ORDER BY ?item'.format(p=p)
         title = '<span id="Single value"></span>\n== "Single value" violations =='
         variables = ['values']
 
-    elif template.name == 'Constraint:Multi values':
-        query = 'SELECT DISTINCT ?item (COUNT(?value) AS ?count) { ?item wdt:{p} ?value .}} GROUP BY ?item HAVING (?count=1) ORDER BY ?item'.format(p=p)
+    elif constrainttype == 'Q21510857': # Constraint:Multi values
+        query = 'SELECT DISTINCT ?item (COUNT(?value) AS ?count) {{ ?item wdt:{p} ?value .}} GROUP BY ?item HAVING (?count=1) ORDER BY ?item'.format(p=p)
         title = '<span id="Multi value"></span>\n== "Multi value" violations =='
 
-    elif template.name == 'Constraint:Item':
-        property = template.get('property').value
-        if template.has('item') or template.has('items'):
-            list = template.get('item').value.split(',') if template.has('item') else template.get('items').value.split(',')
-            list = map(unicode.strip, list)
+    elif constrainttype == 'Q21503247': # Constraint:Item
+        property = constraint.qualifiers['P2306'][0].getTarget().getID()
+        if 'P2305' in constraint.qualifiers:
+            list = [val.getTarget().getID() for val in constraint.qualifiers['P2305']]
             items = 'wd:' + ', wd:'.join(list)
             query = 'SELECT DISTINCT ?item WHERE {{ ?item wdt:{p} [] . MINUS {{ ?item wdt:{property} ?values . FILTER (?values IN ({items})) }} }}'.format(p=p, property=property, items=items)
         else:
             query = 'SELECT DISTINCT ?item WHERE {{ ?item wdt:{p} [] . MINUS {{ ?item wdt:{property} [] }} }}'.format(p=p, property=property)
         title = '<span id="Item {0}"></span>\n== "Item {{{{P|{0}}}}}" violations =='.format(property)
 
-    elif template.name == 'Constraint:Target required claim':
-        property = template.get('property').value
-        if template.has('item') or template.has('items'):
-            list = template.get('item').value.split(',') if template.has('item') else template.get('items').value.split(',')
-            list = map(unicode.strip, list)
+    elif constrainttype == 'Q21510864': # Constraint:Target required claim
+        property = constraint.qualifiers['P2306'][0].getTarget().getID()
+        if 'P2305' in constraint.qualifiers:
+            list = [val.getTarget().getID() for val in constraint.qualifiers['P2305']]
             items = 'wd:' + ', wd:'.join(list)
             query = 'SELECT DISTINCT ?item WHERE {{ ?item wdt:{p} ?target . MINUS {{ ?target wdt:{property} ?values . FILTER (?values IN ({items})) }} }}'.format(p=p, property=property, items=items)
         else:
             query = 'SELECT DISTINCT ?item WHERE {{ ?item wdt:{p} ?target . MINUS {{ ?target wdt:{property} [] }} }}'.format(p=p, property=property)
         title = '<span id="Target required claim {0}"></span>\n== "Target required claim {{{{P|{0}}}}}" violations =='.format(property)
 
-    elif template.name == 'Constraint:Units':
-        list = template.get('list').value.split(',')
-        list = map(unicode.strip, list)
-        units = 'wd:' + ', wd:'.join(list)
-        units = units.replace('none', 'Q199')
-        units = units.replace('novalue', 'Q199')
+    elif constrainttype == 'Q21514353': # Constraint:Units
+        if constraint.qualifiers['P2305'][0].snaktype == 'novalue':
+            units = 'wd:Q199'
+        else:
+            list = [val.getTarget().getID() for val in constraint.qualifiers['P2305']]
+            units = 'wd:' + ', wd:'.join(list)
         query = 'SELECT DISTINCT ?item ?unit WHERE {{ ?item p:{p}/psv:{p}/wikibase:quantityUnit ?unit FILTER (?unit NOT IN ({units})) }} ORDER BY ?item'.format(p=p, units=units)
         title = '<span id="Units"></span>\n== "Units" violations =='
         variables = ['unit']
 
-    elif template.name == 'Constraint:Range':
-        min = template.get('min').value
-        max = template.get('max').value
+    elif constrainttype == 'Q21510860': # Constraint:Range
         if datatype == 'time':
-            if max == 'now':
+            if constraint.qualifiers['P2310'][0].snaktype == 'somevalue':
+                min = time.strftime('%Y')
+            else:
+                min = constraint.qualifiers['P2310'][0].getTarget().year
+            if constraint.qualifiers['P2311'][0].snaktype == 'somevalue':
                 max = time.strftime('%Y')
+            else:
+                max = constraint.qualifiers['P2311'][0].getTarget().year
             query = 'SELECT DISTINCT ?item ?value WHERE {{ ?item wdt:{p} ?value FILTER (year(?value) < {min} || year(?value) > {max}) }} ORDER BY ?item'.format(p=p, min=min, max=max)
         else:
+            min = constraint.qualifiers['P2313'][0].getTarget()
+            max = constraint.qualifiers['P2312'][0].getTarget()
             query = 'SELECT DISTINCT ?item ?value WHERE {{ ?item wdt:{p} ?value FILTER (?value < {min} || ?value > {max}) }} ORDER BY ?item'.format(p=p, min=min, max=max)
         title = '<span id="Range"></span>\n== "Range" violations =='
         variables = ['value']
 
-    elif template.name == 'Constraint:Qualifiers':
-        list = template.get('list').value.split(',')
-        list = map(unicode.strip, list)
-        if template.has('required'):
-            qualifiers = 'pq:' + ' []; pq:'.join(list)
-            query = 'SELECT DISTINCT ?item WHERE {{ ?item p:{p} ?statement . MINUS {{ ?statement {qualifiers} [] }} }} ORDER BY ?item'.format(p=p, qualifiers=qualifiers)
+    elif constrainttype == 'Q21510851': # Constraint:Allowed Qualifiers
+        if constraint.qualifiers['P2306'][0].snaktype == 'novalue':
+            query = 'SELECT DISTINCT ?item WHERE {{ hint:Query hint:optimizer "None" . ?item p:{p} ?statement . ?statement ?pq_qual ?pq_obj . ?qual wikibase:qualifier ?pq_qual }} ORDER BY ?item'.format(p=p)
         else:
+            list = [val.getTarget().getID() for val in constraint.qualifiers['P2306']]
             qualifiers = 'wd:' + ', wd:'.join(list)
             query = 'SELECT DISTINCT ?item WHERE {{ hint:Query hint:optimizer "None" . ?item p:{p} ?statement . ?statement ?pq_qual ?pq_obj . ?qual wikibase:qualifier ?pq_qual . FILTER (?qual NOT IN ({qualifiers})) }} ORDER BY ?item'.format(p=p, qualifiers=qualifiers)
-        title = '<span id="Qualifiers"></span>\n== "Qualifiers" violations =='
+        title = '<span id="Allowed Qualifiers"></span>\n== "Allowed Qualifiers" violations =='
 
-    elif template.name == 'Constraint:Value only':
+    elif constrainttype == 'Q21510856': # Constraint:Mandatory Qualifiers
+        list = [val.getTarget().getID() for val in constraint.qualifiers['P2306']]
+        qualifiers = 'pq:' + ' []; pq:'.join(list)
+        query = 'SELECT DISTINCT ?item WHERE {{ ?item p:{p} ?statement . MINUS {{ ?statement {qualifiers} [] }} }} ORDER BY ?item'.format(p=p, qualifiers=qualifiers)
+        title = '<span id="Mandatory Qualifiers"></span>\n== "Mandatory Qualifiers" violations =='
+
+    elif constrainttype == 'Q21528958': # Constraint:Value only
         query = 'SELECT DISTINCT ?item WHERE {{ {{ ?statement0 pq:{p} ?value . ?item ?p0 ?statement0 . ?prop wikibase:claim ?p0 . FILTER(?prop != wd:P1855 && ?prop != wd:P2271) }} UNION {{ ?ref pr:{p} ?value . ?statement1 prov:wasDerivedFrom ?ref . ?item ?p1 ?statement1 }} }} ORDER BY ?item'.format(p=p)
         title = '<span id="Value only"></span>\n== "Value only" violations =='
 
-    elif template.name == 'Constraint:Qualifier':
+    elif constrainttype == 'Q21510863': # Constraint:Qualifier
         query = 'SELECT DISTINCT ?item WHERE {{ {{ ?item wdt:{p} ?value . }} UNION {{ ?ref pr:{p} ?value . ?statement1 prov:wasDerivedFrom ?ref . ?item ?p1 ?statement1  }} }} ORDER BY ?item'.format(p=p)
         title = '<span id="Qualifier"></span>\n== "Qualifier" violations =='
 
-    elif template.name == 'Constraint:Source only':
+    elif constrainttype == 'Q21528959': # Constraint:Source only
         query = 'SELECT DISTINCT ?item {{ {{ ?item wdt:{p} ?value . }} UNION { ?statement1 pq:{p} ?value . ?item ?p1 ?statement1 }} }} ORDER BY ?item'.format(p=p)
         title = '<span id="Source only"></span>\n== "Source only" violations =='
 
-    elif template.name == 'Constraint:Format':
-        pattern = template.get('pattern').value.strip().replace('<nowiki>', '').replace('</nowiki>', '').replace('\\', '\\\\').replace('"', '\\"')
+    elif constrainttype == 'Q21502404': # Constraint:Format
+        pattern = constraint.qualifiers['P1793'][0].getTarget().replace('\\', '\\\\').replace('"', '\\"')
         if pattern[0] == '^':
             pattern = pattern[1:]
         if pattern[-1] == '$':
@@ -168,9 +175,10 @@ def oneConstraint(p, datatype, template):
         title = '<span id="Format"></span>\n== "Format" violations =='
         variables = ['value']
 
-    elif template.name == 'Constraint:One of':
-        list = template.get('values').value.split(',')
-        list = map(unicode.strip, list)
+    elif constrainttype == 'Q21510859': # Constraint:One of
+        list = [val.getTarget() for val in constraint.qualifiers['P2305']]
+        if None in list: list.remove(None)
+        list = [l.getID() for l in list]
         if datatype == 'wikibase-item':
             values = 'wd:' + ', wd:'.join(list)
         else:
@@ -186,7 +194,7 @@ def oneConstraint(p, datatype, template):
         try:
             data, limit = requestquery(query, 5000)
         except:
-            logwrite('error with '+p+' : '+str(template.name) + '\n' + query + '\n')
+            logwrite('error with ' + p + ' : ' + constrainttype + '\n' + query + '\n')
             return ''
 
     violations = []
@@ -194,7 +202,7 @@ def oneConstraint(p, datatype, template):
     for m in data['results']['bindings']:
         qid = m['item']['value'].replace('http://www.wikidata.org/entity/', '')
         link = 1
-        if template.name == 'Constraint:Unique value':
+        if constrainttype == 'Q21502410':
             if datatype != 'wikibase-item':
                 link = 0
             qid = qid.replace('http://commons.wikimedia.org/wiki/Special:FilePath/', '')
@@ -242,31 +250,26 @@ def main():
         try:
             p = property.title().replace('Property:', '')       # property id
             ppage = pywikibot.PropertyPage(repo, p)             # property page
-            tpage = pywikibot.Page(site, 'Property talk:'+p)    # property talk page
-            #  if tpage.exists() and ppage.exists() #  deactivated due to pywikibot bug
-            if tpage.exists():
-                ppage.get()
-                talkpage = re.sub(r'{{(q|Q)\|q?Q?(\d+)}}', r'Q\2', tpage.get())
-                talkpage = re.sub(r'{{(p|P)\|p?P?(\d+)}}', r'P\2', talkpage)
-                wikicode = mwparserfromhell.parse(talkpage)
-                templates = wikicode.filter_templates()
-                report = u''
-                for t in templates:
-                    if t.name in types:
-                        try:
-                            report += oneConstraint(p, ppage.type, t)  # create report for one constraint
-                        except Exception, e:
-                            logwrite(str(e) + '\n')
-                if len(report) > 1:
+            ppage.get()
+            if 'P2302' not in ppage.claims:
+                continue
+            report = u''
+            for co in ppage.claims['P2302']:
+                if co.getTarget().getID() in types:
                     try:
-                        query = 'SELECT (count(DISTINCT(?item)) AS ?cnt) WHERE {{ {{ ?item wdt:{p} [] . }} UNION {{ ?ref pr:{p} [] . ?statement1 prov:wasDerivedFrom ?ref . ?item ?p1 ?statement1 }} UNION {{ ?statement1 pq:{p} [] . ?item ?p1 ?statement1 }} }}'.format(p=p)
-                        data, _ = requestquery(query)
-                        cnt = data['results']['bindings'][0]['cnt']['value']
-                    except:
-                        cnt = ''
-                    report = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)"), cnt) + report
-                    cpage = pywikibot.Page(site, 'Wikidata:Database reports/Constraint violations/'+p)
-                    cpage.put(report, comment='report update for [[Property:'+p+']]', minorEdit=False)
+                        report += oneConstraint(p, ppage.type, co)  # create report for one constraint
+                    except Exception, e:
+                        logwrite(str(e) + '\n')
+            if len(report) > 1:
+                try:
+                    query = 'SELECT (count(DISTINCT(?item)) AS ?cnt) WHERE {{ {{ ?item wdt:{p} [] . }} UNION {{ ?ref pr:{p} [] . ?statement1 prov:wasDerivedFrom ?ref . ?item ?p1 ?statement1 }} UNION {{ ?statement1 pq:{p} [] . ?item ?p1 ?statement1 }} }}'.format(p=p)
+                    data, _ = requestquery(query)
+                    cnt = data['results']['bindings'][0]['cnt']['value']
+                except:
+                    cnt = ''
+                report = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)"), cnt) + report
+                cpage = pywikibot.Page(site, 'Wikidata:Database reports/Constraint violations/'+p)
+                cpage.put(report, comment='report update for [[Property:'+p+']]', minorEdit=False)
         except Exception, e:
             logwrite(str(e) + '\n')
 
