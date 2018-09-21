e546f84a2f600cb79b768324ce9dcc9a89964284
Pascalco <pascal.leimer@bluewin.ch>
2018-09-21 15:41:38 +0200

    chnge update policy

--- constraints.py
+++ constraints.py
@@ -273,7 +273,9 @@ def main():
             cpage = pywikibot.Page(site, 'Wikidata:Database reports/Constraint violations/'+p)
             if cpage.exists():
                 lastedit = cpage.getVersionHistory()[0]
-                if lastedit[2] == 'KrBot2' and (datetime.datetime.now()-lastedit[1]).days < 5:
+                if (datetime.datetime.now()-lastedit[1]).days < 1:
+                    continue
+                if lastedit[2] == 'KrBot2' and 'ERROR:' not in cpage.text:
                     continue
             report = u''
             for co in ppage.claims['P2302']:
