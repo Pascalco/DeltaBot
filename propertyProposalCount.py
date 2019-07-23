166a773517c814a59c2ad1c1ed47eef6143868c7
Pascalco <Pascalco@users.noreply.github.com>
2019-07-23 22:34:27 +0200

    python3 ready; style

--- propertyProposalCount.py
+++ propertyProposalCount.py
@@ -5,15 +5,15 @@
 import pywikibot
 import re
 
-site = pywikibot.Site('wikidata','wikidata')
+site = pywikibot.Site('wikidata', 'wikidata')
 
 categories = ['Generic', 'Place', 'Authority control', 'Creative work', 'Transportation', 'Person', 'Natural science', 'Organization', 'Sister projects', 'Sports', 'Lexemes']
 text = '<includeonly>{{#switch: {{{1}}}\n'
 for category in categories:
     cnt = 0
-    page = pywikibot.Page(site,'Wikidata:Property_proposal/'+category)
+    page = pywikibot.Page(site, 'Wikidata:Property_proposal/'+category)
     fo = page.get().split('</noinclude>')
-    proposals = re.findall('{{Wikidata:Property proposal/(.*)}}',fo[1])
+    proposals = re.findall('{{Wikidata:Property proposal/(.*)}}', fo[1])
     if proposals:
         for proposal in proposals:
             page2 = pywikibot.Page(site,'Wikidata:Property proposal/'+proposal)
@@ -21,9 +21,9 @@ for category in categories:
                 page2 = page2.getRedirectTarget()
             if not page2.exists():
                 continue
-            cnt += re.sub(r'(<!([^>]+)>)|\s|\n','',page2.get()).count('status=|')
+            cnt += re.sub(r'(<!([^>]+)>)|\s|\n', '', page2.get()).count('status=|')
 
     text += '| '+category+' = '+str(cnt)+'\n'
 text += '| #default = <strong class="error">invalid parameter <kbd>{{{1}}}</kbd></strong>\n}}</includeonly>'
-page = pywikibot.Page(site,'Wikidata:Property proposal/count')
-page.put(text.decode('UTF-8'),comment='upd',minorEdit=False)
+page = pywikibot.Page(site, 'Wikidata:Property proposal/count')
+page.put(text, comment='upd', minorEdit=False)
