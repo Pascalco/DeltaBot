4349fcd59f18762c41f97f6a63f572d2d676c7bc
Michael Scherer <misc@redhat.com>
2020-03-26 11:47:11 +0100

    Use more pythonic code, shorter to read

--- propertyProposalArchive.py
+++ propertyProposalArchive.py
@@ -104,7 +104,7 @@ def main():
                     status = status if status != 'not done' else ''
                     history = page2.getVersionHistory()
                     if (today - history[0].timestamp).days >= 1:
-                        month = str(history[0].timestamp.month) if history[0].timestamp.month > 9 else '0'+str(history[0].timestamp.month)
+                        month = '{:02d}'.format(history[0].timestamp.month)
                         data = {
                             'name': proposal.replace('_', ' '),
                             'newname': newname,
