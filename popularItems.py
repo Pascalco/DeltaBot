fa372280825e1049173265c9b4652faaf36b0308
Pascalco <pascal.leimer@bluewin.ch>
2020-04-24 09:38:04 +0200

    fix

--- popularItems.py
+++ popularItems.py
@@ -20,12 +20,12 @@ img = None
 t1 = date.today().strftime('%Y%m%d%H0000')
 t2 = (date.today() - timedelta(days=2)).strftime('%Y%m%d%H0000')
 cur = db.cursor()
-cur.execute('SELECT rc_title, COUNT(*) AS cnt FROM recentchanges LEFT JOIN change_tag ON rc_id = ct_rc_id WHERE ct_tag_id IS NULL AND rc_source="mw.edit" AND rc_bot=0 AND rc_namespace=0 AND rc_patrolled<>0 AND rc_timestamp<%s AND rc_timestamp>%s AND rc_old_len<rc_new_len AND rc_title NOT IN (SELECT pl_title FROM pagelinks WHERE pl_from = 26001882 AND pl_namespace = 0) GROUP BY rc_title HAVING COUNT(DISTINCT rc_actor) >= 3 ORDER BY cnt DESC' % (t1,t2))
+cur.execute('SELECT rc_title, COUNT(*) AS cnt FROM recentchanges WHERE rc_source="mw.edit" AND rc_bot=0 AND rc_namespace=0 AND rc_patrolled<>0 AND rc_timestamp<{} AND rc_timestamp>{} AND rc_old_len<rc_new_len AND rc_title NOT IN (SELECT pl_title FROM pagelinks WHERE pl_from = 26001882 AND pl_namespace = 0) GROUP BY rc_title HAVING COUNT(DISTINCT rc_actor) >= 3 ORDER BY cnt DESC'.format(t1,t2))
 for row in cur.fetchall():
-    q = row[0]
+    q = row[0].decode()
     if q in blacklist:
         continue
-    r = requests.get('https://www.wikidata.org/w/api.php?action=wbgetclaims&entity=%s&format=json' % q)
+    r = requests.get('https://www.wikidata.org/w/api.php?action=wbgetclaims&entity={}&format=json'.format(q))
     data = r.json()
     if 'error' in data:
         continue
@@ -42,7 +42,7 @@ for row in cur.fetchall():
             if 'P18' in data['claims']:
                 if data['claims']['P18'][0]['mainsnak']['snaktype'] == 'value':
                     img = data['claims']['P18'][0]['mainsnak']['datavalue']['value']
-                    text = '<span style="float:right; padding-top:0.5em; padding-left:0.5em;">[[File:%s|100px]]</span>\n%s ({{I18n|pictured}})' % (img.encode('utf-8'),text)
+                    text = '<span style="float:right; padding-top:0.5em; padding-left:0.5em;">[[File:{}|100px]]</span>\n{} ({{{{I18n|pictured}}}})'.format(img, text)
     i += 1
     if i == 7:
         break
@@ -50,6 +50,5 @@ for row in cur.fetchall():
 if not img:
     text = '<nowiki></nowiki>\n' + text
 text += '<span style="clear:right;"></span>'
-#page = pywikibot.Page(site, 'Wikidata:Main Page/Popular')
-page = pywikibot.Page(site, 'User:Pasleim/test')
+page = pywikibot.Page(site, 'Wikidata:Main Page/Popular')
 page.put(text, comment='upd', minorEdit=False)
