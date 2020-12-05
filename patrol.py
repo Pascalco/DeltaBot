233101f363732035c0d5e451ff40b692e45df916
Pascalco <pascal.leimer@bluewin.ch>
2020-12-05 10:19:14 +0100

    autopatrol certain items

--- patrol.py
+++ patrol.py
@@ -5,13 +5,15 @@
 import pywikibot
 from datetime import timedelta, datetime
 
+autopatrol = ['Q4115189', 'Q13406268', 'Q15397819', 'Q16943273', 'Q17339402', 'Q17566023', 'Q17578745', 'Q85408509', 'Q85408938', 'Q85409163', 'Q85409310', 'Q85409446', 'Q85409596']
+
 start = datetime.utcnow() - timedelta(minutes=15)
 starttime = start.strftime('%Y%m%d%H%M%S')
 
 site = pywikibot.Site('wikidata', 'wikidata')
 gen = site.recentchanges
-generator = gen(start=starttime, bot=False, patrolled=True, reverse=True)
 
+generator = gen(start=starttime, bot=False, patrolled=True, reverse=True)
 for rev in generator:
     try:
         if '/* restore' in rev['comment']:
@@ -24,6 +26,16 @@ for rev in generator:
         elif '/* undo' in rev['comment']:
             comment = rev['comment'].split('|')
             p = site.patrol(revid=comment[2])
+            next(p)
+    except:
+        pass
+
+
+generator = gen(start=starttime, bot=False, patrolled=False, reverse=True)
+for rev in generator:
+    try:
+        if rev['title'] in autopatrol:
+            p = site.patrol(revid=rev['revid'])
             next(p)
     except:
         pass
