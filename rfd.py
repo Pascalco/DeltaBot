0029fdd8688a54fc46d93d0233d7d51be0536d7d
Pascalco <pascal.leimer@bluewin.ch>
2020-12-20 12:35:17 +0100

    more error prone

--- rfd.py
+++ rfd.py
@@ -16,36 +16,39 @@ force = False
 
 content = re.findall(r'(?:(?<!=)==([^=]+)==(?!=))?([\s\S]+?(?=$|(?<!=)==[^=]+==(?!=)))', page.get())
 for i in range(len(content)):
-    content[i] = list(map(str.strip, list(content[i])))
-    res = re.search(r'(Q\d+)', content[i][0])
-    if res:
-        entity = pywikibot.ItemPage(repo, res.group(1))
-    else:
-        res = re.search(r'(Lexeme:L\d+)', content[i][0])
+    try:
+        content[i] = list(map(str.strip, list(content[i])))
+        res = re.search(r'(Q\d+)', content[i][0])
         if res:
-            entity = pywikibot.Page(repo, res.group(1))  # T189321
-    if res:
-        if any(x in content[i][1] for x in ('{{done', '{{deleted', '{{not done', '{{notdone', '{{not deleted', '{{merged')):
-            continue
-        if not entity.exists() and not entity.isRedirectPage():
-            for m in site.logevents(logtype='delete', page=entity, total=1):
-                content[i][1] += u'\n: {{{{deleted|admin={}}}}} --~~~~'.format(m.user())
-            cntDone += 1            
-        elif entity.isRedirectPage() and entity.getRedirectTarget().exists():
-            content[i][1] += (u'\n: {{{{done}}}} Redirect created by [[User:{}]], you can do it ' +
-                              u'[[Special:MyLanguage/Help:Merge|yourself]] next time. --~~~~').format(entity.userName())
-            cntDone += 1
+            entity = pywikibot.ItemPage(repo, res.group(1))
         else:
-            if '{{on hold' not in content[i][1]:
-                refs = list(entity.backlinks(followRedirects=False, filterRedirects=False, namespaces=[0, 120], total=12))
-                numberOfRefs = len(refs)
-                if entity in refs:
-                    numberOfRefs -= 1
-                if numberOfRefs > 0:
-                    force = True
-                    content[i][1] += u'\n: {{{{on hold}}}} This item is linked from {}{} other{}. --~~~~'.format(
-                        min(numberOfRefs, 10), '+' if numberOfRefs > 10 else '', 's' if numberOfRefs > 1 else '')
-            cntNotDone += 1
+            res = re.search(r'(Lexeme:L\d+)', content[i][0])
+            if res:
+                entity = pywikibot.Page(repo, res.group(1))  # T189321
+        if res:
+            if any(x in content[i][1] for x in ('{{done', '{{deleted', '{{not done', '{{notdone', '{{not deleted', '{{merged')):
+                continue
+            if not entity.exists() and not entity.isRedirectPage():
+                for m in site.logevents(logtype='delete', page=entity, total=1):
+                    content[i][1] += u'\n: {{{{deleted|admin={}}}}} --~~~~'.format(m.user())
+                cntDone += 1            
+            elif entity.isRedirectPage() and entity.getRedirectTarget().exists():
+                content[i][1] += (u'\n: {{{{done}}}} Redirect created by [[User:{}]], you can do it ' +
+                                  u'[[Special:MyLanguage/Help:Merge|yourself]] next time. --~~~~').format(entity.userName())
+                cntDone += 1
+            else:
+                if '{{on hold' not in content[i][1]:
+                    refs = list(entity.backlinks(followRedirects=False, filterRedirects=False, namespaces=[0, 120], total=12))
+                    numberOfRefs = len(refs)
+                    if entity in refs:
+                        numberOfRefs -= 1
+                    if numberOfRefs > 0:
+                        force = True
+                        content[i][1] += u'\n: {{{{on hold}}}} This item is linked from {}{} other{}. --~~~~'.format(
+                            min(numberOfRefs, 10), '+' if numberOfRefs > 10 else '', 's' if numberOfRefs > 1 else '')
+                cntNotDone += 1
+    except:
+        pass
 
 text = ''
 for section in content:
