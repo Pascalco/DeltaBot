8cb60c48ec470b7e3d06d7371276f43ae9aac696
Pascalco <pascal.leimer@bluewin.ch>
2021-08-02 20:27:12 +0200

    fix

--- popularItems.py
+++ popularItems.py
@@ -82,7 +82,7 @@ for q in sorted:
             if 'P18' in data['claims']:
                 if data['claims']['P18'][0]['mainsnak']['snaktype'] == 'value':
                     img = data['claims']['P18'][0]['mainsnak']['datavalue']['value']
-                    text = '<span style="float: {{dir|{{{lang|{{int:lang}}}}}|left|right}}; padding-top: 0.5em; padding-{{dir|{{{lang|{{int:lang}}}}}|right|left}}: 0.5em;">[[File:%s|100px]]</span>\n%s ({{{{I18n|pictured}}}})' % (img, text)
+                    text = '<span style="float: {{dir|{{{lang|{{int:lang}}}}}|left|right}}; padding-top: 0.5em; padding-{{dir|{{{lang|{{int:lang}}}}}|right|left}}: 0.5em;">[[File:%s|100px]]</span>\n%s ({{I18n|pictured}})' % (img, text)
     i += 1
     if i == 7:
         break
