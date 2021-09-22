0a42f663cf5a6f210cf2d048cbc4c430ba502888
Pascalco <pascal.leimer@bluewin.ch>
2021-09-22 20:36:08 +0200

    include qualifiers

--- somevalue.py
+++ somevalue.py
@@ -10,15 +10,39 @@ site = pywikibot.Site('wikidata', 'wikidata')
 repo = site.data_repository()
 
 whitelist = ['Q108474139', 'Q53569537']
-
 tasks = {'somevalue': 'Q53569537', 'novalue': 'Q108474139'}
+
+
+def testSnak(snak, task):
+    if snak['datatype'] != 'wikibase-item':
+        return False
+    if 'datavalue' not in snak:
+        return False
+    if snak['datavalue']['value']['id'] != tasks[task]:
+        return False
+    return True
+
+def move(claim, task):
+    mydata = {}
+    mydata['claims'] = [claim]
+    item = pywikibot.ItemPage(repo, q)
+    item.editEntity(mydata, summary=u'move claim [['+tasks[task]+']] -> '+task)
+
 
 for task in tasks:
     payload = {
-        'query': 'SELECT ?item WHERE { ?item ?p wd:'+tasks[task]+' . ?p ^wikibase:directClaim [] . } ',
+        'query': '''SELECT ?item WHERE {{
+            ?item ?p ?statement .
+            ?statement ?pq wd:'''+tasks[task]+''' .
+            ?p ^wikibase:claim [] .
+            ?pq ^wikibase:qualifier []
+            } UNION {
+            ?item ?p wd:Q108474139 .
+            ?p ^wikibase:directClaim [] .
+            }
+        }''',
         'format': 'json'
     }
-    print(payload)
     r = requests.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql?', params=payload)
     data = r.json()
     for m in data['results']['bindings']:
@@ -31,13 +55,16 @@ for task in tasks:
             continue
         for p in data['entities'][q]['claims']:
             for m in data['entities'][q]['claims'][p]:
-                if not 'datavalue' in m['mainsnak']:
-                    continue
-                if m['mainsnak']['datatype'] == 'wikibase-item':
-                    if m['mainsnak']['datavalue']['value']['id'] == tasks[task]:
-                        del m['mainsnak']['datavalue']
-                        m['mainsnak']['snaktype'] = task
-                        mydata = {}
-                        mydata['claims'] = [m]
-                        item = pywikibot.ItemPage(repo, q)
-                        item.editEntity(mydata, summary=u'move claim [['+tasks[task]+']] -> '+task)
+                if testSnak(m['mainsnak'], task):
+                    del m['mainsnak']['datavalue']
+                    m['mainsnak']['snaktype'] = task
+                    move(m, task)
+                if 'qualifiers' in m:
+                    for qp in m['qualifiers']:
+                        for quali in m['qualifiers'][qp]:
+                            testSnak(quali, task)
+                            if quali['datatype'] == 'wikibase-item' and 'datavalue' in quali:
+                                if quali['datavalue']['value']['id'] == tasks[task]:
+                                    del quali['datavalue']
+                                    quali['snaktype'] = task
+                                    move(m, task)
